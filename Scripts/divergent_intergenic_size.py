23c4a4119f7466832f29a58e29a16c6cf1657fbc
ibioChile <53570955+ibioChile@users.noreply.github.com>
2020-07-31 10:07:36 -0400

    Update divergent_intergenic_size.py

--- Scripts/divergent_intergenic_size.py
+++ Scripts/divergent_intergenic_size.py
@@ -1,13 +1,17 @@
 from statistics import mean, stdev
+import pandas as pd
 
 prev_chrom = ''
 prev_strand = ''
+prev_gene_id = ''
 inter_size_list = []
+inter_size_df = []
 
 gff_bot = open("Botrytis_cinerea.ASM83294v1.47.genes.bed",'r')
 
 for current_line in gff_bot:
     L = current_line.strip().split()
+    gene_id = str(L[3]).split(":")[1]
     chromosome = L[0]
     strand = L[5]
     start = L[1]
@@ -19,10 +23,14 @@ for current_line in gff_bot:
             stop_inter = start
             inter_size = float(start) - float(prev_end)
             inter_size_list.append(inter_size)
-
+            inter_size_df.append([prev_gene_id, gene_id, inter_size])
     prev_chrom = chromosome
     prev_strand = strand
+    prev_gene_id = gene_id
     prev_start = start
     prev_end = end
 
 print("Average of Divergent intergenic size: " + str(round(mean(inter_size_list))) + "+-" + str(round(stdev(inter_size_list))) + "bp" )
+
+df = pd.DataFrame(inter_size_df, columns=['Gene1', 'Gene2', 'Intergenic size (bp)'])
+df.to_csv(r'/Users/pamelacamejo/Documents/IBIO/Paulo_Canessa/FIMO_Botrytis/Results/Intergenic_Divergent_Genes_Bot.csv', index=False)
